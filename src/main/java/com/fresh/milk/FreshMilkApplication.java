c069843c6a17673f753ae17d7743c87f6903bb13
ljg <1056705837@qq.com>
2021-09-14 21:59:18 +0800

    首次提交

---
+++ src/main/java/com/fresh/milk/FreshMilkApplication.java
@@ -0,0 +1,13 @@
+package com.fresh.milk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshMilkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshMilkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fresh/milk/FreshMilkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresh.milk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshMilkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
